ed80358c5682a36061899d8bd0c57e11334c0596
devbelly <devbelly@naver.com>
2022-09-15 11:29:19 +0900

    jdbcCursorItemReader

---
+++ src/main/java/io/spring/springbatch/Customer.java
@@ -0,0 +1,12 @@
+package io.spring.springbatch;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String birthdate;
+}
